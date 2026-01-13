63026c4bf62a6624fef38c135316cdeae41f6da5
Hannah Waruguru <hannah.waruguru8@gmail.com>
2026-01-13 19:48:39 +0300

    Add express server, REST API and static client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-
+import express from "express"
+import cors from "cors"
+import path from "path"
 import { Database } from "./db/database.js"
 
 
+const app = express()
 const db = new Database()
 
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(process.cwd(), "client")))
+app.get("/users", (req, res)=> res.json(db.execute("SELECT * FROM users")))
+app.post("/users", (req, res)=>{
+    const {id,name,email} = req.body
+    db.execute(`INSERT INTO users VALUES (${id}, ${name}, ${email})`)
+    res.send("ok")
+})
+
+app.listen(3000, ()=>console.log("Sever running on port 3000"))
 console.log("RDBMS started");
